79925a2dd6ab1ba9e5cd19ad0d823868ef332485
Abhishek Warokar <abhishek11@live.in>
2017-10-08 14:28:50 +0530

    :tada: working!

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -144,10 +144,21 @@ function populateCandidateVotes() {
 	}
 }
 
+// Dummy Article Data 
+const articleTitle = [
+	'We saved money on inauguration thanks to the dam\'s collapse: Nitish Kumar',
+	'Mortified Tampax CEO Bursts Into Tears And Runs Out Of Boardroom After Tampon Falls Out Of Briefcase',
+	'BJP asks next election be held on Twitter after Congress loses series of Twitter polls',
+	'Anti-Nuclear Weapons Organization Awarded Nobel Peace Prize',
+	'Why is Google News still running fake news sources like CNN and WaPo while banning REAL news from the new media?'
+];
+
+var i = 0;
+
 function setupCandidateRows() {
 	Object.keys(candidates).forEach(function (candidate) {
 		$("#candidate-rows").append(
-			"<tr><td>" + candidate + "</td><td id='" + candidates[candidate] + "'></td><td><a href='#' onclick='voteForCandidate(0)' class='btn btn-warning'>FAKE</a><a href='#' onclick='voteForCandidate(1)' class='btn btn-success' style='margin-left: 12px'>GENUINE</a></td></tr>");
+			"<tr><td>" + (i + 1) + "</td><td>" + articleTitle[i++] + "</td><td id='" + candidates[candidate] + "'></td><td><a href='#' onclick='voteForCandidate(0)' class='btn btn-warning'>FAKE</a><a href='#' onclick='voteForCandidate(1)' class='btn btn-success' style='margin-left: 12px'>GENUINE</a></td></tr>");
 	});
 }
 
